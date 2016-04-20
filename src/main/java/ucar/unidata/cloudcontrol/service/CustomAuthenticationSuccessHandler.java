1cbd7300c17a1ce20cb2237a85280e0a1022073a
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2016-04-19 20:11:06 -0600

    removed unused vars

--- src/main/java/ucar/unidata/cloudcontrol/service/CustomAuthenticationSuccessHandler.java
+++ src/main/java/ucar/unidata/cloudcontrol/service/CustomAuthenticationSuccessHandler.java
@@ -1,42 +1,28 @@
 package edu.ucar.unidata.cloudcontrol.service;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Logger;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import java.io.IOException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 /**
  * Service class used to handle a successful user authentication.
  */
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-	
-	protected static Logger logger = Logger.getLogger(CustomAuthenticationSuccessHandler.class);
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-		Cookie[] cookie = httpServletRequest.getCookies();
 
-        HttpSession session = httpServletRequest.getSession();
-        User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String userName = authUser.getUsername();
-		
         //set our response to OK status
         httpServletResponse.setStatus(HttpServletResponse.SC_OK);
 
         //since we have created our custom success handler, its up to us to where
         //we will redirect the user after successfully login
-        httpServletResponse.sendRedirect("welcome");
+        httpServletResponse.sendRedirect("dashboard");
     }
 }
