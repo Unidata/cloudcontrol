bf6ff856df8fb1e1ca68743d4cb7c20497b2fe72
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2017-06-15 15:35:48 -0600

    test applicationContext.xml

--- src/test/java/edu/ucar/unidata/cloudcontrol/categories/UnitTest.java
+++
@@ -1,8 +0,0 @@
-package edu.ucar.unidata.cloudcontrol.categories;
-
-/**
- * This is a marker interface that identifies tests which
- * belong to the unit test suite.
- */
-public interface UnitTest {
-}
