b20bbb50cbe5d2bcb3ae4a04a547dcad4dda123e
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2017-06-28 16:00:11 -0600

    more methods for UserBuilder

--- src/test/java/edu/ucar/unidata/cloudcontrol/domain/UserBuilder.java
+++ src/test/java/edu/ucar/unidata/cloudcontrol/domain/UserBuilder.java
@@ -1,5 +1,6 @@
 package edu.ucar.unidata.cloudcontrol.domain;
 
+import java.util.Date;
 
 public class UserBuilder {
 
@@ -16,6 +17,46 @@ public class UserBuilder {
 
     public UserBuilder userName(String userName) {
         user.setUserName(userName);
+        return this;
+    }
+
+    public UserBuilder fullName(String fullName) {
+        user.setFullName(fullName);
+        return this;
+    }
+
+    public UserBuilder emailAddress(String emailAddress) {
+        user.setEmailAddress(emailAddress);
+        return this;
+    }
+
+    public UserBuilder password(String password) {
+        user.setPassword(password);
+        return this;
+    }
+
+    public UserBuilder confirmPassword(String confirmPassword) {
+        user.setConfirmPassword(confirmPassword);
+        return this;
+    }
+
+    public UserBuilder accessLevel(int accessLevel) {
+        user.setAccessLevel(accessLevel);
+        return this;
+    }
+
+    public UserBuilder accountStatus(int accountStatus) {
+        user.setAccountStatus(accountStatus);
+        return this;
+    }
+
+    public UserBuilder dateCreated(Date dateCreated) {
+        user.setDateCreated(dateCreated);
+        return this;
+    }
+
+    public UserBuilder dateModified(Date dateModified) {
+        user.setDateModified(dateModified);
         return this;
     }
 
