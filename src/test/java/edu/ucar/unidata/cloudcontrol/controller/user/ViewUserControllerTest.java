aee57c9e42b595297afa05262ac5ba6a58faf880
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2017-06-28 16:02:32 -0600

    removal of unused imports

--- src/test/java/edu/ucar/unidata/cloudcontrol/controller/user/ViewUserControllerTest.java
+++ src/test/java/edu/ucar/unidata/cloudcontrol/controller/user/ViewUserControllerTest.java
@@ -2,11 +2,9 @@ package edu.ucar.unidata.cloudcontrol.controller.user;
 
 import edu.ucar.unidata.cloudcontrol.config.SecurityConfig;
 import edu.ucar.unidata.cloudcontrol.config.WebAppContext;
-
-import edu.ucar.unidata.cloudcontrol.service.user.UserManager;
-
 import edu.ucar.unidata.cloudcontrol.domain.User;
 import edu.ucar.unidata.cloudcontrol.domain.UserBuilder;
+import edu.ucar.unidata.cloudcontrol.service.user.UserManager;
 
 import java.util.Arrays;
 
@@ -15,20 +13,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.mockito.Mockito;
-import org.mockito.stubbing.OngoingStubbing;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.dao.RecoverableDataAccessException;
-
 import org.springframework.security.test.context.support.WithMockUser;
-
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.allOf;
@@ -45,17 +38,11 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
-import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 //import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -72,10 +59,10 @@ public class ViewUserControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private WebApplicationContext context;
+    private UserManager userManagerMock;
 
     @Autowired
-    private UserManager userManagerMock;
+    private WebApplicationContext context;
 
     @Before
     public void setUp() {
@@ -94,7 +81,7 @@ public class ViewUserControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("denied"))
             .andExpect(forwardedUrl("/WEB-INF/views/denied.jsp"));
-            //.andDo(print());
+          //.andDo(print());
     }
 
     @Test
@@ -104,7 +91,7 @@ public class ViewUserControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("dashboard"))
             .andExpect(forwardedUrl("/WEB-INF/views/dashboard.jsp"));
-            //.andDo(print());
+          //.andDo(print());
     }
 
     @Test
@@ -112,7 +99,7 @@ public class ViewUserControllerTest {
     public void testListUsers_ModelShouldContainListUsersAction() throws Exception {
         mockMvc.perform(get("/dashboard/user").with(csrf()))
             .andExpect(model().attribute("action", equalTo("listUsers")));
-            //.andDo(print());
+          //.andDo(print());
     }
 
     @Test
@@ -144,7 +131,7 @@ public class ViewUserControllerTest {
                     hasProperty("userName", is("testUserTwo"))
                 )
             )));
-            //.andDo(print());
+          //.andDo(print());
             verify(userManagerMock, times(1)).getUserList();
             verifyNoMoreInteractions(userManagerMock);
     }
@@ -156,7 +143,7 @@ public class ViewUserControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("denied"))
             .andExpect(forwardedUrl("/WEB-INF/views/denied.jsp"));
-            //.andDo(print());
+          //.andDo(print());
     }
 
     @Test
@@ -166,7 +153,7 @@ public class ViewUserControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("dashboard"))
             .andExpect(forwardedUrl("/WEB-INF/views/dashboard.jsp"));
-            //.andDo(print());
+          //.andDo(print());
     }
 
     @Test
@@ -176,7 +163,7 @@ public class ViewUserControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("dashboard"))
             .andExpect(forwardedUrl("/WEB-INF/views/dashboard.jsp"));
-            //.andDo(print());
+          //.andDo(print());
     }
 
 
@@ -193,7 +180,7 @@ public class ViewUserControllerTest {
         mockMvc.perform(get("/dashboard/user/view/testUserOne").with(csrf()))
             .andExpect(model().attribute("action", equalTo("viewUser")))
             .andExpect(model().attribute("user", theInstance(testUserOne)));
-            //.andDo(print());
+          //.andDo(print());
             verify(userManagerMock, times(1)).lookupUser("testUserOne");
             verifyNoMoreInteractions(userManagerMock);
     }
@@ -207,7 +194,7 @@ public class ViewUserControllerTest {
             .andExpect(status().isOk())
             .andExpect(view().name("fatalError"))
             .andExpect(forwardedUrl("/WEB-INF/views/fatalError.jsp"));
-            //.andDo(print());
+          //.andDo(print());
             verify(userManagerMock, times(1)).lookupUser("testUserOne");
             verifyNoMoreInteractions(userManagerMock);
     }
