adfbde5b6d5561adde74af132a0224755da0cd16
Jennifer Oxelson <oxelson@unidata.ucar.edu>
2017-07-07 13:53:52 -0600

    fixed exception handling

--- src/main/java/edu/ucar/unidata/cloudcontrol/controller/docker/ClientController.java
+++ src/main/java/edu/ucar/unidata/cloudcontrol/controller/docker/ClientController.java
@@ -44,7 +44,6 @@ import edu.ucar.unidata.cloudcontrol.service.docker.validators.ClientConfigValid
 /**
  * Controller to handle the Docker client configuration and commands.
  */
-
 @Controller
 public class ClientController implements HandlerExceptionResolver {
 
@@ -52,188 +51,198 @@ public class ClientController implements HandlerExceptionResolver {
 
     @Resource(name = "clientManager")
     private ClientManager clientManager;
-    
+
     @Autowired
     private ClientConfigValidator clientConfigValidator;
-    
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         StringTrimmerEditor stringtrimmer = new StringTrimmerEditor(true);
         binder.registerCustomEditor(String.class, stringtrimmer);
-        binder.setValidator(clientConfigValidator);  
-    }   
-    
+        binder.setValidator(clientConfigValidator);
+    }
+
     /**
-     * Accepts a GET request to create a new ClientConfig object. 
+     * Accepts a GET request to create a new ClientConfig object.
      *
-     * The view is the dashboard.  The model contains a blank ClientConfig and
-     * action information which will be loaded and displayed in the view via jspf.
-     * If we already have configuration data in the database, the model contains the 
-     * requested ClientConfig displayed in the view via jspf.
-     * 
-     * @param authentication  The Authentication object to check roles with. 
+     * The view is the dashboard.
+     *
+     * Only authenticated users are allowed access to this view.
+     *
+     * If the user has role of 'ROLE_ADMIN' and there are no configs in the database,
+     * the model contains an empty ClientConfig object and a configureClient action
+     * which will loaded the configureClient.jspf into the dashboard. Otherwise, if 
+     * configuration data is already in the database, the model contains the requested 
+     * ClientConfig object and no action is specifies, hence status.jspf is loaded.
+     *
+     * If the user does not have the role of 'ROLE_ADMIN', the model contains no action
+     * or ClientConfig object and status.jspf is loaded into the dashboard view.
+     *
+     * @param authentication  The Authentication object to check roles with.
      * @param model  The Model used by the View.
      * @return  The path for the ViewResolver.
      */
+    @PreAuthorize("isAuthenticated()")
     @RequestMapping(value = "/dashboard", method = RequestMethod.GET)
     public String getDashboardPage(Authentication authentication, Model model) {
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         if (authorities.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
-	        List<ClientConfig> clientConfigs = clientManager.getAllClientConfigs();
-	        if (clientConfigs.size() > 0) {
-	            try {
-	                ClientConfig clientConfig = clientConfigs.get(0);        
-	                model.addAttribute("clientConfig", clientConfig);      
-	            } catch (RecoverableDataAccessException e) {
-	                throw new RuntimeException("Unable to find clientConfig: " +  e);
-	            }
-	        } else {          
-	            ClientConfig clientConfig = new ClientConfig();
-	            model.addAttribute("action", "configureClient");  
-	            model.addAttribute("clientConfig", clientConfig); 
-	        }
+            List<ClientConfig> clientConfigs = clientManager.getAllClientConfigs();
+            if (clientConfigs.size() > 0) {
+                // the following for now until we implement connections to multiple docker instances
+                ClientConfig clientConfig = clientConfigs.get(0);
+                model.addAttribute("clientConfig", clientConfig);
+            } else {
+                ClientConfig clientConfig = new ClientConfig();
+                model.addAttribute("action", "configureClient");
+                model.addAttribute("clientConfig", clientConfig);
+            }
         }
         return "dashboard";
     }
-    
+
+    /**
+     * Accepts a GET request to create a new ClientConfig object.
+     *
+     * The view is the dashboard.  If there are no configs in the database, the
+     * model contains an empty ClientConfig object and a configureClient action
+     * which will loaded the configureClient.jspf into the dashboard. Otherwise,
+     * if configuration data is already in the database, the model contains the
+     * requested ClientConfig object and the viewClientConfig action which loads
+     * the viewClientConfig.jspf into the dashbaord view.
+     *
+     * Only users with a role of 'ROLE_ADMIN' are allowed access to this view.
+     *
+     * @param model  The Model used by the View.
+     * @return  The path for the ViewResolver.
+     */
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(value = "/dashboard/docker/client/configure", method = RequestMethod.GET)
+    public String configure(Model model) {
+        List<ClientConfig> clientConfigs = clientManager.getAllClientConfigs();
+        if (clientConfigs.size() > 0) {
+            // the following for now until we implement connections to multiple docker instances
+            ClientConfig clientConfig = clientConfigs.get(0);
+            model.addAttribute("clientConfig", clientConfig);
+            model.addAttribute("action", "viewClientConfig");
+        } else {
+            ClientConfig clientConfig = new ClientConfig();
+            model.addAttribute("action", "configureClient");
+            model.addAttribute("clientConfig", clientConfig);
+        }
+        return "dashboard";
+    }
+
     /**
      * Accepts a GET request for a List of all ClientConfig objects.
      *
      * The view is the dashboard.  The model contains a List of ClientConfig
      * objects which will be loaded and displayed in the view via jspf.
-     * The view can handle an empty list of ClientConfigs if no ClientConfig objects 
+     * The view can handle an empty list of ClientConfigs if no ClientConfig objects
      * have been persisted in the database yet.
      *
      * Only users with the role of 'ROLE_ADMIN' can view the list of ClientConfigs.
-     * 
+     *
      * @param model  The Model used by the View.
      * @return  The path for the ViewResolver.
      */
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value="/dashboard/docker/client", method=RequestMethod.GET)
-    public String listClientConfigs(Model model) { 
+    public String listClientConfigs(Model model) {
         List<ClientConfig> clientConfigs = clientManager.getAllClientConfigs();
-        model.addAttribute("action", "listClientConfigs"); 
+        model.addAttribute("action", "listClientConfigs");
+        // the following for now until we implement connections to multiple docker instances
         if (clientConfigs.size() > 0) {
-            model.addAttribute("clientConfig", clientConfigs.get(0)); 
-        }            
+            model.addAttribute("clientConfig", clientConfigs.get(0));
+        }
         return "dashboard";
     }
-    
+
     /**
      * Accepts a GET request to retrieve a specific ClientConfig object.
      *
-     * View is the dashboard.  The model contains the 
+     * View is the dashboard.  The model contains the
      * requested ClientConfig displayed in the view via jspf.
-     *    
+     *
      * Only users with the role of 'ROLE_ADMIN' can see Docker client configuration information.
-     * 
+     *
      * @param id  The id as provided by @PathVariable.
      * @param model  The Model used by the View.
      * @return  The path for the ViewResolver.
-     * @throws RuntimeException  If unable to find the logged-in ClientConfig's info.
+     * @throws RuntimeException  If unable to find the requested ClientConfig.
      */
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value="/dashboard/docker/client/view/{id}", method=RequestMethod.GET)
-    public String viewClientConfig(@PathVariable int id, Model model) { 
+    public String viewClientConfig(@PathVariable int id, Model model) {
         try {
-            ClientConfig clientConfig = clientManager.lookupById(id);           
-            model.addAttribute("clientConfig", clientConfig);    
-            model.addAttribute("action", "viewClientConfig");    
+            ClientConfig clientConfig = clientManager.lookupById(id);
+            model.addAttribute("clientConfig", clientConfig);
+            model.addAttribute("action", "viewClientConfig");
             return "dashboard";
         } catch (RecoverableDataAccessException e) {
             throw new RuntimeException("Unable to find clientConfig with id: " +  new Integer(id).toString());
         }
     }
-    
+
     /**
-     * Accepts a GET request to create a new ClientConfig object. 
-     *
-     * The view is the dashboard.  The model contains a blank ClientConfig and
-     * action information which will be loaded and displayed in the view via jspf.
-     * If we already have configuration data in the database, the model contains the 
-     * requested ClientConfig displayed in the view via jspf.
-     *
-     * Only users with a role of 'ROLE_ADMIN' are allowed access
-     * to this view. 
-     * 
-     * @param model  The Model used by the View.
-     * @return  The path for the ViewResolver.
-     */
-    @RequestMapping(value = "/dashboard/docker/client/configure", method = RequestMethod.GET)
-    public String configure(Model model) {
-        List<ClientConfig> clientConfigs = clientManager.getAllClientConfigs();
-        if (clientConfigs.size() > 0) {
-            try {
-                ClientConfig clientConfig = clientConfigs.get(0);        
-                model.addAttribute("clientConfig", clientConfig);    
-                model.addAttribute("action", "viewClientConfig");    
-                return "dashboard";
-            } catch (RecoverableDataAccessException e) {
-                throw new RuntimeException("Unable to find clientConfig: " +  e);
-            }
-        } else {          
-            ClientConfig clientConfig = new ClientConfig();
-            model.addAttribute("action", "configureClient");  
-            model.addAttribute("clientConfig", clientConfig); 
-        }
-        return "dashboard";
-    }
-    
-    /**
-     * Accepts a POST request to create a new ClientConfig object and persist it. 
+     * Accepts a POST request to create a new ClientConfig object and persist it.
      *
      * View is the dashboard.  The model contains:
      * 1) the newly created ClientConfig object (if successful) displayed in the view via jspf; or
-     * 2) the web form to create a new ClientConfig if there are validation errors with the user input. 
+     * 2) the web form to create a new ClientConfig if there are validation errors with the user input.
      *
      * Only users with the role of 'ROLE_ADMIN' can configure a Docker Client.
-     * 
-     * @param clientConfig  The ClientConfig to persist. 
+     *
+     * @param clientConfig  The ClientConfig to persist.
      * @param result  The BindingResult for error handling.
      * @param model  The Model used by the View.
-     * @return  The redirect to the needed View. 
+     * @return  The redirect to the needed View.
      * @throws RuntimeException  If unable to create new ClientConfig.
      */
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value="/dashboard/docker/client/configure", method=RequestMethod.POST)
-    public ModelAndView configure(@Valid ClientConfig clientConfig, BindingResult result, Model model) { 
+    public ModelAndView createClientConfig(@Valid ClientConfig clientConfig, BindingResult result, Model model) {
         if (result.hasErrors()) {
-           model.addAttribute("action", "configureClient");  
-           model.addAttribute("clientConfig", clientConfig);   
-           return new ModelAndView("dashboard"); 
+           model.addAttribute("action", "configureClient");
+           model.addAttribute("clientConfig", clientConfig);
+           return new ModelAndView("dashboard");
         } else {
             try {
                 UserDetails authUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
                 clientConfig.setCreatedBy(authUser.getUsername());
                 clientConfig.setLastUpdatedBy(authUser.getUsername());
-                int id = clientManager.createClientConfig(clientConfig);  
-                return new ModelAndView(new RedirectView("/dashboard/docker/client/view/" + new Integer(id).toString(), true)); 
+                int id = clientManager.createClientConfig(clientConfig);
+                return new ModelAndView(new RedirectView("/dashboard/docker/client/view/" + new Integer(id).toString(), true));
             } catch (RecoverableDataAccessException e) {
                 throw new RuntimeException("Unable to create new ClientConfig: " + e);
             }
-        }         
+        }
     }
-    
+
     /**
-     * Accepts a GET request to edit an existing ClientConfig object. 
-     * 
+     * Accepts a GET request to edit an existing ClientConfig object.
+     *
      * The view is the dashboard.  The model contains the ClientConfig object to edit
      * and the information which will be loaded and displayed in the view via jspf.
      *
      * Only users with the role of 'ROLE_ADMIN' can edit Docker Client Configuration information.
-     * 
+     *
      * @param id  The id as provided by @PathVariable.
      * @param model  The Model used by the View.
      * @return  The path for the ViewResolver.
+     * @throws RuntimeException  If unable to find the requested ClientConfig.
      */
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value="/dashboard/docker/client/edit/{id}", method=RequestMethod.GET)
-    public String editClientConfig(@PathVariable int id, Model model) {   
-        ClientConfig clientConfig = clientManager.lookupById(id);           
-        model.addAttribute("clientConfig", clientConfig);    
-        model.addAttribute("action", "editClientConfig");
-        return "dashboard";   
+    public String editClientConfig(@PathVariable int id, Model model) {
+        try {
+            ClientConfig clientConfig = clientManager.lookupById(id);
+            model.addAttribute("clientConfig", clientConfig);
+            model.addAttribute("action", "editClientConfig");
+            return "dashboard";
+        } catch (RecoverableDataAccessException e) {
+            throw new RuntimeException("Unable to find clientConfig with id: " +  new Integer(id).toString());
+        }
     }
 
     /**
@@ -244,9 +253,9 @@ public class ClientController implements HandlerExceptionResolver {
      * 2) the web form to edit the ClientConfig if there are validation errors with the clientConfig input.
      *
      * Only users with the role of 'ROLE_ADMIN' can edit Docker Client Configuration information.
-     * 
-     * @param id  The id as provided by @PathVariable. 
-     * @param clientConfig  The ClientConfig to edit. 
+     *
+     * @param id  The id as provided by @PathVariable.
+     * @param clientConfig  The ClientConfig to edit.
      * @param result  The BindingResult for error handling.
      * @param model  The Model used by the View.
      * @return  The redirect to the needed View.
@@ -258,9 +267,9 @@ public class ClientController implements HandlerExceptionResolver {
         if (result.hasErrors()) {
             logger.info("an error has occurred");
             model.addAttribute("action", "editClientConfig");
-            model.addAttribute("clientConfig", clientConfig);  
-            return new ModelAndView("dashboard"); 
-        } else {   
+            model.addAttribute("clientConfig", clientConfig);
+            return new ModelAndView("dashboard");
+        } else {
             try {
                 UserDetails authUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
                 clientConfig.setLastUpdatedBy(authUser.getUsername());
@@ -268,22 +277,22 @@ public class ClientController implements HandlerExceptionResolver {
                 clientConfig.setCreatedBy(dbClientConfig.getCreatedBy());
                 clientConfig.setDateCreated(dbClientConfig.getDateCreated());
                 clientManager.updateClientConfig(clientConfig);
-                return new ModelAndView(new RedirectView("/dashboard/docker/client/view/" + new Integer(id).toString(), true));  
+                return new ModelAndView(new RedirectView("/dashboard/docker/client/view/" + new Integer(id).toString(), true));
             } catch (RecoverableDataAccessException e) {
                 throw new RuntimeException("Unable to edit clientConfig: " +  e);
             }
-        }    
+        }
     }
 
     /**
      * This method gracefully handles any uncaught exception
      * that are fatal in nature and unresolvable by the user.
-     * 
+     *
      * @param request   The current HttpServletRequest request.
      * @param response  The current HttpServletRequest response.
-     * @param handler  The executed handler, or null if none chosen at the time of the exception.  
+     * @param handler  The executed handler, or null if none chosen at the time of the exception.
      * @param exception  The  exception that got thrown during handler execution.
-     * @return  The error page containing the appropriate message to the dockerImage. 
+     * @return  The error page containing the appropriate message to the dockerImage.
      */
     @Override
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) {
@@ -294,20 +303,20 @@ public class ClientController implements HandlerExceptionResolver {
         printWriter.flush();
 
         String stackTrace = writer.toString();
-        
+
         ModelAndView modelAndView = new ModelAndView();
         Map<String, Object> model = new HashMap<String, Object>();
-        if (exception instanceof AccessDeniedException){ 
+        if (exception instanceof AccessDeniedException){
             message = exception.getMessage();
             modelAndView.setViewName("denied");
         } else  {
-            message = "An error has occurred: " + exception.getClass().getName() + ": " + stackTrace;  
-            modelAndView.setViewName("fatalError"); 
+            message = "An error has occurred: " + exception.getClass().getName() + ": " + stackTrace;
+            modelAndView.setViewName("fatalError");
         }
-        logger.error(message);       
+        logger.error(message);
         model.put("message", message);
         modelAndView.addAllObjects(model);
         return modelAndView;
-    } 
+    }
 
 }
